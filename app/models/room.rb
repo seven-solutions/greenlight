e4900142312c532a1aa12f88b3304fd1f663c9ba
Lorenz Sieben <me@lorenz-sieben.com>
2020-09-13 12:33:09 +0200

    Do not leak personal information in URLS

--- app/models/room.rb
+++ app/models/room.rb
@@ -114,9 +114,9 @@ class Room < ApplicationRecord
   # Generates a fully random room uid.
   def random_room_uid
     # 6 character long random string of chars from a..z and 0..9
-    full_chunk = SecureRandom.alphanumeric(9).downcase
+    full_chunk = SecureRandom.alphanumeric(15).downcase
 
-    [owner.name_chunk, full_chunk[0..2], full_chunk[3..5], full_chunk[6..8]].join("-")
+    full_chunk
   end
 
   # Generates a unique bbb_id based on uuid.
